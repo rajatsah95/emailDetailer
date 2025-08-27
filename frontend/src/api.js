f47aa70051ce363f672d809f774e10a43f73cfce
rajatsah95 <rajatsah95@gmail.com>
2025-08-27 13:32:47 +0530

    API_BASE value modified in api.js in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_BASE =
-  import.meta.env.VITE_API_BASE ||
-  "https://emaildetailerbackend-1.onrender.com/";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
 
 export async function getTestInfo() {
   return axios.get(`${API_BASE}/test-info`).then((r) => r.data);
